bbec76d276462a0d25c8f4b093bbe5b0c0e32f37
profgonzagas <profgonzagas@gmail.com>
2024-11-07 09:14:47 -0300

    Navegando para a tela CadDetailActivity

--- app/src/main/java/com/example/myapitest/CarDetailActivity.kt
+++ app/src/main/java/com/example/myapitest/CarDetailActivity.kt
@@ -7,26 +7,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapitest.databinding.ActivityCarDetailBinding
 
 class CarDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCarDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_car_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityCarDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       // enableEdgeToEdge()
+      //  setContentView(R.layout.activity_car_detail)
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+        setupView()
+    }
+
+    private fun setupView() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
         }
     }
+
     companion object {
         private const val ARG_ID = "ARG_ID"
         fun newIntent(
             context: Context,
             itemId: String
         ) =
-            Intent(context, CarDetailActivity::class.java).apply {
-                putExtra(ARG_ID, itemId)
+            Intent(context, CarDetailActivity::class.java).apply {//acesse a nova tela e aplique
+                putExtra(ARG_ID, itemId) //chave e valor
             }
     }
 }
